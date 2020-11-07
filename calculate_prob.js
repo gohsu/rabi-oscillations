f5437ebcfd3f99e675dcd6510c6456369b68bbcc
Christian Hellum Bye <christian.hellum.bye@gmail.com>
2020-11-07 15:50:45 -0500

    fix more syntax because i am incompetent

--- calculate_prob.js
+++ calculate_prob.js
@@ -1,43 +1,42 @@
-yarn add @tensorflow/tfjs-node
-// <script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@1.0.0/dist/tf.min.js"></script>
+//yarn add @tensorflow/tfjs-node
+//<script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@1.0.0/dist/tf.min.js"></script>
+
+const tf = require('@tensorflow/tfjs');
+
 
 function ab_to_alphabeta(thet) {
-    const M;
-    M = tf.constant([[tf.math.cos(thet/2), tf.math.sin(thet/2)],[tf.math.sin(thet/2),-tf.math.cos(thet/2)]]);
+    const M = tf.constant([[tf.math.cos(thet/2), tf.math.sin(thet/2)],[tf.math.sin(thet/2),-tf.math.cos(thet/2)]]);
     return M;
 }
 
 function theta(w0, w) {
-    const thet;
-    thet = tf.math.acos((w0-w)/weff);
+    const thet = tf.math.acos((w0-w)/weff);
     return thet;
 }
 
 function params_to_omegas(B0, B, q, m) {
-    const g, c, w0, w1;
-    g = 2.002
-    c = 29979245800 // cgs!
-    w0 = -g*q*B0/(2*m*c);
-    w1 = -g*q*B/(2*m*c);
+    const g = 2.002
+    const c = 29979245800 // cgs!
+    const w0 = -g*q*B0/(2*m*c);
+    const w1 = -g*q*B/(2*m*c);
     return w0, w1;
 }
 
 function psi(t, psi0, w0, w1, w) {
-    const thet, weff, M, alpha_beta, alpha, beta, costo2, sinto2, psi_t;
     var top, bot;
-    thet = theta(w0,w1,w);
-    weff = tf.math.sqrt((w0-w)**2 + w1**2); 
-    M = ab_to_alphabeta(thet);
-    alpha_beta = tf.matmul(M, psi0);
-    alpha = alpha_beta[0];
-    beta = alpha_beta[1];
-    costo2 = tf.math.cos(thet/2);
-    sinto2 = tf.math.sin(thet/2);
-    top = costo2*tf.math.exp(-1j*w/2*t)*tf.math.exp(-1j*weff/2*t)*alpha;
-    top += sinto2*tf.math.exp(-1j*w/2*t)*tf.math.exp(1j*weff/2*t)*beta;
-    bot = sinto2*tf.math.exp(1j*w/2*t)*tf.math.exp(-1j*weff/2*t)*alpha;
-    bot += -costo2*tf.math.exp(1j*w/2*t)*tf.math.exp(1j*weff/2*t)*beta;
-    psi_t = tf.constant([[top],[bot]],dtype=complex)
+    const thet = theta(w0,w1,w);
+    const weff = tf.math.sqrt((w0-w)**2 + w1**2); 
+    const M = ab_to_alphabeta(thet);
+    const alpha_beta = tf.matmul(M, psi0);
+    const alpha = alpha_beta[0];
+    const beta = alpha_beta[1];
+    const costo2 = tf.math.cos(thet/2);
+    const sinto2 = tf.math.sin(thet/2);
+    top = costo2*tf.math.exp(tf.complex(0, -w/2*t))*tf.math.exp(tf.complex(0, -weff/2*t))*alpha;
+    top += sinto2*tf.math.exp(tf.complex(0, -w/2*t))*tf.math.exp(tf.complex(0, weff/2*t))*beta;
+    bot = sinto2*tf.math.exp(tf.complex(0, w/2*t))*tf.math.exp(tf.complex(0, -weff/2*t))*alpha;
+    bot += -costo2*tf.math.exp(tf.complex(0, w/2*t))*tf.math.exp(tf.complex(0, weff/2*t))*beta;
+    const psi_t = tf.constant([[top],[bot]],dtype=complex)
     return psi_t;
 } 
 
@@ -55,3 +54,7 @@ function main(t, psi0, B0, B, w, m) {
     prob = probability_plus_z(psi_t);
     return prob;
 }
+
+const psi0 = tf.constant([[1],[0]])
+var prob = main(1, psi0, 1, 1, 1, 1);
+print(prob);
