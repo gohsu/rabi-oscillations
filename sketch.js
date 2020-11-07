85de5c40c8b4680f4a5d8c797cb7d978a5654d05
cclin130 <cheng.lin103@gmail.com>
2020-11-07 12:40:26 -0500

    styles

--- sketch.js
+++ sketch.js
@@ -1,12 +1,19 @@
 function setup() {
-    createCanvas(400, 400);
+  var cnv = createCanvas(windowWidth - 200, 500);
+  cnv.style('display', 'block');
+  background(255, 0, 200);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth - 200, 500);
+}
+
+function draw() {
+  background(255, 0, 200);
+  if (mouseIsPressed) {
+    fill(0);
+  } else {
+    fill(255);
   }
-  
-  function draw() {
-    if (mouseIsPressed) {
-      fill(0);
-    } else {
-      fill(255);
-    }
-    ellipse(mouseX, mouseY, 80, 80);
-  }
+  ellipse(mouseX, mouseY, 80, 80);
+}
