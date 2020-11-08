a4d13c6c382631a99aa13fd9888354ec7b3bacd9
Christian Hellum Bye <christian.hellum.bye@gmail.com>
2020-11-07 20:04:32 -0500

    change from tf to math

--- calculate_prob.js
+++ calculate_prob.js
@@ -1,21 +1,19 @@
-const tf = require('@tensorflow/tfjs');
-
+//const tf = require('@tensorflow/tfjs');
+const math = require('mathjs');
 
 function ab_to_alphabeta(thet) {
-    const a = tf.cos(tf.div(thet,2));
-    const b = tf.sin(tf.div(thet,2));
-    const c = tf.mul(-1, a);
-    const row1 = a.concat(b);
-    const row2 = b.concat(c);
-    const M = tf.stack([row1, row2]);
+    const a = math.cos(math.divide(thet,2));
+    const b = math.sin(math.divide(thet,2));
+    const c = math.multiply(-1, a);
+    const M = math.matrix([[a, b], [b, c]]);
     return M;
 }
 
 function theta(w0, w1, w) {
-    const t1 = tf.sub(w0, w);
-    const weff_sq = tf.add(t1.pow(2), w1.pow(2));
-    const weff = weff_sq.sqrt();
-    const thet = tf.acos(tf.div(t1, weff));
+    const t1 = math.subtract(w0, w);
+    const weff_sq = math.add(math.pow(t1, 2), math.pow(w1, 2));
+    const weff = math.sqrt(weff_sq);
+    const thet = math.acos(math.divide(t1, weff));
     return thet;
 }
 
@@ -25,42 +23,42 @@ function params_to_omegas(B0, B, q, m) {
     const c = 1;
     const w0 = -g*q*B0/(2*m*c);
     const w1 = -g*q*B/(2*m*c);
-    return tf.tensor([w0, w1]);
+    return [w0, w1];
 }
 
 function psi_top(t, psi0, w0, w1, w) {
     const thet = theta(w0,w1,w);
-    const t1 = tf.sub(w0, w);
-    const weff_sq = tf.add(t1.pow(2), w1.pow(2));
-    const weff = weff_sq.sqrt();
+    const t1 = math.subtract(w0, w);
+    const weff_sq = math.add(math.pow(t1, 2), math.pow(w1, 2));
+    const weff = math.sqrt(weff_sq);
     const M = ab_to_alphabeta(thet);
-    const alpha_beta = tf.dot(M, psi0);
-    const alpha = tf.slice(alpha_beta, 0, 1);
-    const beta = tf.slice(alpha_beta, 1, 1);
-    const term1 = tf.pow(tf.mul(alpha, tf.cos(tf.div(thet,2))),2);
-    const term2 = tf.pow(tf.mul(beta, tf.sin(tf.div(thet,2))), 2)
-    const term3 = tf.mul(alpha, beta, tf.sin(thet), tf.cos(weff * t));
-    const psi_t = tf.add(term1, term2, term3);
+    const alpha_beta = math.multiply(M, psi0);
+    const alpha = alpha_beta['_data'][0];
+    const beta = alpha_beta['_data'][1];
+    const term1 = math.pow(alpha * math.cos(math.divide(thet,2)),2);
+    const term2 = math.pow(beta * math.sin(math.divide(thet,2)), 2)
+    const term3 = alpha * beta * math.sin(thet) * math.cos(weff * t);
+    const psi_t = math.add(term1, term2, term3);
     return psi_t;
 } 
 
 function main(t, real_row1_psi0, im_row1_psi0, real_row2_psi0, im_row2_psi0, B0, B, w, m, q) {
     const w0_w1 = params_to_omegas(B0,B,q,m);
-    const w0 = tf.slice(w0_w1, 0, 1);
-    const w1 = tf.slice(w0_w1, 1, 1);
-    const norm_psi0 = tf.sqrt(tf.add(real_row1_psi0.pow(2), im_row1_psi0.pow(2), real_row2_psi0.pow(2), im_row2_psi0.pow(2))); 
-    const psi0row1 = tf.complex(tf.div(real_row1_psi0, norm_psi0), tf.div(im_row1_psi0, norm_psi0));
-    const psi0row2 = tf.complex(tf.div(real_row2_psi0, norm_psi0), tf.div(im_row2_psi0, norm_psi0));
-    const psi0 = tf.stack([psi0row1, psi0row2]); 
+    const w0 = w0_w1[0];
+    const w1 = w0_w1[1];
+    const norm_psi0 = math.sqrt(math.add(math.pow(real_row1_psi0, 2), math.pow(im_row1_psi0, 2), math.pow(real_row2_psi0, 2), math.pow(im_row2_psi0, 2))); 
+    const psi0row1 = math.complex(math.divide(real_row1_psi0, norm_psi0), math.divide(im_row1_psi0, norm_psi0));
+    const psi0row2 = math.complex(math.divide(real_row2_psi0, norm_psi0), math.divide(im_row2_psi0, norm_psi0));
+    const psi0 = math.transpose(math.matrix([psi0row1, psi0row2]));
     const psi_t = psi_top(t,psi0,w0,w1,w);
     return psi_t;
 }
 
-const real_row1_psi0 = tf.tensor([1]);
-const im_row1_psi0 = tf.tensor([0]);
-const real_row2_psi0 = tf.tensor([0]);
-const im_row2_psi0 = tf.tensor([0]);
-const omega = tf.tensor([1]);
+const real_row1_psi0 = 0;
+const im_row1_psi0 = 0;
+const real_row2_psi0 = 1;
+const im_row2_psi0 = 0;
+const omega = 1;
 var prob = main(0, real_row1_psi0, im_row1_psi0, real_row2_psi0, im_row2_psi0, 0, 0, omega, 1, -1);
 console.log('prob');
-prob.print();
+console.log(prob);
